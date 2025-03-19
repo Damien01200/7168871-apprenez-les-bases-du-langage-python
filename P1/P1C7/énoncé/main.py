624f2ab8adf6310da0bd9d443a2b2a7a7b58370a
Damien01200 <damiensaad0@gmail.com>
2025-03-19 23:02:17 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,22 @@
-# Écrivez votre code ici !
+#Creation dictionnaire
+
+fruits={"pomme":"rouge",
+        "banane":"jaune",
+        "orange":"orange"}
+fruits["kiwi"]="vert"
+print(fruits)
+
+#Valeur cle banane
+couleur_banane = fruits["banane"]
+print(couleur_banane)
+
+#Valeur associé à la cle pomme pour vert
+fruits["pomme"]= "vert" 
+print(fruits)
+#Supression cle banane du dictionnaire fruits
+
+del fruits["banane"]
+print(fruits)
+#Afficher cles restante
+
+print(fruits.keys())
