267aee5ed9cfa8f1cf0f41432b21decdc8e3746b
Damien01200 <damiensaad0@gmail.com>
2025-03-19 22:07:38 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+#Definition variable
+Nom = "Damien"
+Age = 25
+Taille = 1,70
+est_etudiant = False
+#Affichage nom age taille est_etudiant
+Les prints doivent être sous la forme suivantes :
+
+print(f"Type nom: {type(Nom)}")
+print(f"Type age: {type(Age)}")
+print(f"Type taille: {type(Taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
