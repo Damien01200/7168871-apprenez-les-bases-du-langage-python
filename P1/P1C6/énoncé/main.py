01ec94c4afb9d78d10f7b493c92015683dd61d64
Damien01200 <damiensaad0@gmail.com>
2025-03-19 22:28:08 +0100

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,16 @@
-# Écrivez votre code ici !
+#Creation liste
+fruit = ["pomme","banane","orange"]
+#Ajout kiwi a la liste
+fruit.append("kiwi")
+print(fruit)
+#Supression orange
+fruit.remove("orange")
+print(fruit)
+#Changement deuxieme element banane en annanas
+fruit[1]="ananas"
+print (fruit)
+#Longueur liste fruit
+print(len(fruit))
+#Tri par ordre alphabetique
+fruit.sort()
+print(fruit)             
